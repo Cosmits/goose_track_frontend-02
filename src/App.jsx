ef36de6eb9e6212493f9e0bd72b23d4f86883542
Gabriella Marusyak <slipuhinag@gmail.com>
2023-10-13 15:35:31 +0300

    Cteated Discription component

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import SecondPage from './pages/SecondPage/SecondPage';
 import HalfPage from './pages/HalfPage/HalfPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import { AppWrapper } from './App.styled';
+import { Discription } from './components/Discription/Discription';
+
 
 const test = import.meta.env.VITE_API_TEST;
 
@@ -18,14 +20,15 @@ const test = import.meta.env.VITE_API_TEST;
 function App() {
   console.log(test);
   return (
+    
     <AppWrapper>
+      <Discription />
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route path="/first" element={<FirstPage />} />
           <Route path="/second" element={<SecondPage />}>
             <Route path=":half" element={<HalfPage />} />
           </Route>
-
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
