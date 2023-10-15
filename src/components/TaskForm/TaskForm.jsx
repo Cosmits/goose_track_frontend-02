e62f9936b756c724b0adca7cee54abb96a618dc9
Ruslana Onyshchuk <lalelya7@gmail.com>
2023-10-15 19:22:06 +0200

    added basic form markup

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-
 const TaskForm = () => {
-
-  
-
   return (
     <div>
-      
+      <form>
+        <label>
+          Title
+          <input type="text" placeholder="Enter text" />
+        </label>
+        {/* <label>
+          <input type="text" name="Enter text" />
+        </label> */}
+        <label>
+          <input type="radio" name="color" value="blue" checked />
+          Low
+        </label>
+        <label>
+          <input type="radio" name="color" value="orange" />
+          Medium
+        </label>
+        <label>
+          <input type="radio" name="color" value="green" />
+          High
+        </label>
+        <button type="submit">Edit</button>
+        <button type="buttom">Cancel</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default TaskForm;
