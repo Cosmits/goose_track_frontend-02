ee14ccf6d0ea158b7f74b59186302e2acc9ca2df
Cosmits <salge.cos@gmail.com>
2023-10-17 23:37:02 +0200

    min fix

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,8 @@
-import ReviewsSlider from '../components/MainPage/ReviewsSlider/ReviewsSlider';
-// import LoginForm from "components/LoginForm/LoginForm";
 
 import { Auth } from "../components/authSection/auth";
 import { Description } from "../components/Description/Description";
-import ReviewsSlider from "../components/ReviewsSlider/ReviewsSlider";
+import ReviewsSlider from '../components/MainPage/ReviewsSlider/ReviewsSlider';
+
 
 const MainPage = () => {
   return <>
