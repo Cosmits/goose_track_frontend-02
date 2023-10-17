d9514f9df629506b8bc0a4c123b1da341ab21069
DarinaMerkulova <litovchenko.darina@gmail.com>
2023-10-17 20:41:56 +0300

    add sidebar

--- src/components/Header/MyHeader.jsx
+++ src/components/Header/MyHeader.jsx
@@ -1,39 +1,64 @@
 // import { useEffect, useState } from 'react';
 // import { useSearchParams } from 'react-router-dom';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import UserInfo from './UserInfo/UserInfo';
 import ThemeToggler from './ThemeToggler/ThemeToggler';
-import { BurgerMenu, HeaderUser, HeaderWrapper } from './MyHeader.styled';
+import { BurgerMenu, HeaderCurrentPage, HeaderUser, HeaderWrapper } from './MyHeader.styled';
 import AddFeedbackBtn from './FeedbackBtn/AddFeedbackBtn';
 import { useScreenSize } from '../../hooks/useScreenSize';
+import AddFeedbackModal from '../AddFeedbackModal/AddFeedbackModal';
+import { useEffect, useState } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
 
 
 const MyHeader = () => {
   const {isDesktop} = useScreenSize();
- 
-  // const [title, setTitle] = useState("");
-  // const [searchParams] = useSearchParams();
-  // const searchTerm = searchParams.get('query');
+  const [showModal, setShowModal] = useState(false);
+  const [showSideBar, setShowSideBar] = useState(false);
+  const [currentPage, setCurrentPage] = useState("");
 
-  // useEffect(() => {
-  //   if (!searchTerm) return;
+  const location = useLocation()
+console.log(location)
+  const openModal = () => {
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
+  };
+ const toggleSideBar = () => {
+  setShowSideBar(() => !showSideBar);
 
-  //   setTitle(searchTerm);
-  // }, [searchTerm, setTitle ]);
+ }
+
+ useEffect(() => {
+  const path = location.pathname;
+  switch (path) {
+    case '/calendar':
+      setCurrentPage('Calendar');
+      break;
+    case '/statistics':
+      setCurrentPage('Statistics');
+      break;
+    default:
+      setCurrentPage('User Profile');
+  }
+}, [location.pathname]);
 
   return (
     <HeaderWrapper>
-      {isDesktop&&<h2>Привіт</h2>}
-      {!isDesktop && <BurgerMenu/>}
+      {isDesktop&&<HeaderCurrentPage>{currentPage}</HeaderCurrentPage>}
+      {!isDesktop && <BurgerMenu onClick={toggleSideBar}/>}
       <HeaderUser>
-      <AddFeedbackBtn />
+      <AddFeedbackBtn openModal={openModal}/>
       <ThemeToggler/>
             <Link to="/account">
         <UserInfo />
-      </Link>
-      </HeaderUser>
+      </Link>      </HeaderUser>
+      {showModal &&  <AddFeedbackModal onClose={closeModal} />}
+      {showSideBar && <Sidebar/>}
     </HeaderWrapper>
+     
   );
 };
 
