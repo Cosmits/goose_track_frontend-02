ed732b71ee0c73f3e64a5051752161fa79c52e86
Cosmits <salge.cos@gmail.com>
2023-10-17 23:24:42 +0200

    add main, login, register pages

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -3,8 +3,8 @@
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
 import AuthNavigate from '../../components/AuthNavigate/AuthNavigate';
 import { AuthFormWrap, AuthPageWrap, PictureWrap } from './RegisterPage.styled';
-import gooseRegister from '../../images/goose_register@1x.png';
-import gooseRegisterImg from '../../images/goose_register@2x.png';
+// import gooseRegister from '../../images/goose_register@1x.png';
+// import gooseRegisterImg from '../../images/goose_register@2x.png';
 
 const RegisterPage = () => {
   return (
@@ -16,7 +16,7 @@ const RegisterPage = () => {
 
       <AuthNavigate route="/login" textContent="Log In" />
       <PictureWrap >
-        <picture>
+        {/* <picture>
             <source
               type="image/png"
               srcSet={`${gooseRegister} 1x, ${gooseRegisterImg} 2x`}
@@ -27,7 +27,7 @@ const RegisterPage = () => {
               width={400}
               height={416}
             />
-          </picture>
+          </picture> */}
         </PictureWrap>
     </AuthPageWrap>
     
