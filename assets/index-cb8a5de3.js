b29a9d8f53c5c8fe329513cb29bac06dd8c7b864
Cosmits <salge.cos@gmail.com>
2023-10-17 12:43:35 +0000

    Deploying to gh-pages from @ Cosmits/goose_track_frontend-02@77fd49ec7609bb058d220d0462423caf860a2c18 🚀

--- assets/index-cb8a5de3.js
+++ assets/index-cb8a5de3.js
@@ -387,7 +387,7 @@ Error generating stack: `+o.message+`
   background-image: url(${Wg});
   background-repeat: no-repeat;
   background-position: center bottom;
-  `;function ax(e){return dl({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"}},{tag:"polyline",attr:{points:"10 17 15 12 10 7"}},{tag:"line",attr:{x1:"15",y1:"12",x2:"3",y2:"12"}}]})(e)}const ux=$e.section`
+`;function ax(e){return dl({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"}},{tag:"polyline",attr:{points:"10 17 15 12 10 7"}},{tag:"line",attr:{x1:"15",y1:"12",x2:"3",y2:"12"}}]})(e)}const ux=$e.section`
   box-sizing: border-box;
   margin: -8px;
   display: flex;
