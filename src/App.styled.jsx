e0271fc30660c74c1d1f583ef0acc2fed4df62ab
Illia Shatokhin <resonane.2106@gmail.com>
2023-10-17 15:07:36 +0300

    standarted files

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -4,7 +4,7 @@ import example from './assets/example.png';
 export const AppWrapper = styled.div`
   width: 100%;
   height: 100vh;
-  /* background-image: url(${example}); */
+  background-image: url(${example});
   background-repeat: no-repeat;
   background-position: center bottom;
 `;
