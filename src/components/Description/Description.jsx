2d15be53a64a030cffc64ed17666c9ff826bb214
Cosmits <salge.cos@gmail.com>
2023-10-18 14:17:32 +0200

    fix

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,24 +1,24 @@
-import MediaQuery, { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from 'react-responsive';
 import sidebarImg from '../../images/Description/mobile-sideBar@1x.jpg';
-import sidebarImgRetina from '../../images/Description/mobile-sideBar@2x.jpg'
+import sidebarImgRetina from '../../images/Description/mobile-sideBar@2x.jpg';
 import calendarImg from '../../images/Description/mobile-calendar@1x.jpg';
 import calendarImgRetina from '../../images/Description/mobile-calendar@2x.jpg';
-import taskImg from '../../images/Description/mobile-tasks@1x.jpg'
-import taskImgRetina from '../../images/Description/mobile-tasks@2x.jpg'
+import taskImg from '../../images/Description/mobile-tasks@1x.jpg';
+import taskImgRetina from '../../images/Description/mobile-tasks@2x.jpg';
 
 import tabletSidebarImg from '../../images/Description/tablet-sideBar@1x.jpg';
 import tabletSidebarImgRetina from '../../images/Description/tablet-sideBar@2x.jpg'
 import tabletCalendarImg from '../../images/Description/tablet-calendar@1x.jpg';
 import tabletCalendarImgRetina from '../../images/Description/tablet-calendar@2x.jpg';
-import tabletTaskImg from '../../images/Description/tablet-tasks@1x.jpg'
-import tabletTaskImgRetina from '../../images/Description/tablet-tasks@2x.jpg'
+import tabletTaskImg from '../../images/Description/tablet-tasks@1x.jpg';
+import tabletTaskImgRetina from '../../images/Description/tablet-tasks@2x.jpg';
 
 import deckSidebarImg from '../../images/Description/desk-sideBar@1x.jpg';
-import deskSidebarImgRetina from '../../images/Description/desk-sideBar@2x.jpg'
+import deskSidebarImgRetina from '../../images/Description/desk-sideBar@2x.jpg';
 import deskCalendarImg from '../../images/Description/desk-calendar@1x.jpg';
 import deskCalendarImgRetina from '../../images/Description/desk-calendar@2x.jpg';
-import deskTaskImg from '../../images/Description/desk-tasks@1x.jpg'
-import deskTaskImgRetina from '../../images/Description/desk-tasks@2x.jpg'
+import deskTaskImg from '../../images/Description/desk-tasks@1x.jpg';
+import deskTaskImgRetina from '../../images/Description/desk-tasks@2x.jpg';
 
 import { DescriptionImg, MiddleItem, StyledContainer, StyledDescriptionTitle, StyledDescription, StyledItem, StyledList, StyledNumberPar, StyledPreTitle } from './Description.styled';
 import { useScreenSize } from '../../hooks/useScreenSize';
@@ -35,7 +35,8 @@ export const Description = () => {
                     <StyledNumberPar>1.</StyledNumberPar>
                     <StyledPreTitle>Calendar</StyledPreTitle>
                     <StyledDescriptionTitle>View</StyledDescriptionTitle>
-                    <StyledDescription>GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.</StyledDescription>
+                    <StyledDescription>GooseTrack's Calendar view provides a comprehensive overview of your schedule,
+                        displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.</StyledDescription>
                 </div>
 
                 {isDesktop && <DescriptionImg src={isRetina ? deskCalendarImgRetina : deskCalendarImg} alt="Calendar" />}
@@ -49,7 +50,9 @@ export const Description = () => {
                 <StyledContainer >
                     <StyledNumberPar>2.</StyledNumberPar>
                     <StyledDescriptionTitle>Sidebar</StyledDescriptionTitle>
-                    <StyledDescription>GooseTrack offers easy access to your account settings, calendar, and filters. The "My Account" section allows you to manage your profile information and preferences, while the calendar provides a quick and convenient way to view your upcoming events and tasks.</StyledDescription>
+                    <StyledDescription>GooseTrack offers easy access to your account settings, calendar, and filters.
+                        The "My Account" section allows you to manage your profile information and preferences,
+                        while the calendar provides a quick and convenient way to view your upcoming events and tasks.</StyledDescription>
                 </StyledContainer>
 
                 {isDesktop && <DescriptionImg src={isRetina ? deskSidebarImgRetina : deckSidebarImg} alt="Sidebar" />}
@@ -63,7 +66,8 @@ export const Description = () => {
                     <StyledNumberPar>3.</StyledNumberPar>
                     <StyledPreTitle>All in</StyledPreTitle>
                     <StyledDescriptionTitle>One</StyledDescriptionTitle>
-                    <StyledDescription>GooseTrack is an all-in-one productivity tool that helps you stay on top of your tasks, events, and deadlines. Say goodbye to scattered to-do lists and hello to streamlined productivity with GooseTrack.</StyledDescription>
+                    <StyledDescription>GooseTrack is an all-in-one productivity tool that helps you stay on top of your tasks,
+                        events, and deadlines. Say goodbye to scattered to-do lists and hello to streamlined productivity with GooseTrack.</StyledDescription>
                 </div>
                 {isDesktop && <DescriptionImg src={isRetina ? deskTaskImgRetina : deskTaskImg} alt="Task example" />}
 
