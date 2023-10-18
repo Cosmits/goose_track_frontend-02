359d27dcd3e1b6424b9191263c9a6ac9c1ce46df
Cosmits <salge.cos@gmail.com>
2023-10-18 19:59:24 +0000

    Deploying to gh-pages from @ Cosmits/goose_track_frontend-02@1d528d018dfb521a9530f61f4b6591a3ac0a1326 🚀

--- assets/index-c2d7a865.js
+++ assets/index-c2d7a865.js
@@ -988,6 +988,24 @@ max-height: 700px;
   font-weight: 500;
   line-height: ${()=>18/14};
 
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-track {
+    border-radius: 12px;
+    background: #f2f2f2;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 12px;
+    background: #e7e5e5;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #3e85f3;
+  }
+
   @media screen and (min-width: 768px) {
     margin-left: 68px;
   }
