44ab270451af27cbe72b832bb7a8564ec720ad07
DarinaMerkulova <litovchenko.darina@gmail.com>
2023-10-18 16:24:09 +0300

    calendar Day in header

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,4 +1,5 @@
 import { RxHamburgerMenu } from "react-icons/rx";
+
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
@@ -18,4 +19,28 @@ export const HeaderCurrentPage = styled.h2`
 font-size:32px;
 font-weight: 700;
 line-height: 1;
+`
+export const NoTaskWrapper = styled.div`
+display:flex;
+align-items:center;
+gap: 8px;`
+
+export const NoTaskTitle = styled.p`
+color: #111;
+font-family: Inter;
+font-size: 14px;
+font-weight: 600;
+line-height: 1.28;
+margin-top:8px;
+`
+export const HeaderLink = styled.a`
+color: #3E85F3;
+font-family: Inter;
+font-size: 14px;
+font-weight: 600;
+line-height: 1.28;
+`
+export const LogoHeader = styled.img`
+width: 64px;
+height: 60px;
 `
