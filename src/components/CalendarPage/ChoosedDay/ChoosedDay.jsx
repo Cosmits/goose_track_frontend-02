61140c196abef55a91bec75329bcb08f3ee74f26
Illia Shatokhin <resonane.2106@gmail.com>
2023-10-19 18:34:45 +0300

    chesed day styles finished

---
+++ src/components/CalendarPage/ChoosedDay/ChoosedDay.jsx
@@ -0,0 +1,79 @@
+import { ChoosedDaySection } from './ChoosedDay.styled';
+import TasksColumnsList from './TasksColumnsList/TasksColumnsList';
+
+export default function ChoosedDay() {
+  const filteredTasks = {
+    'To do': [
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Medium',
+        id: '1',
+      },
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Medium',
+        id: '2',
+      },
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Medium',
+        id: '3',
+      },
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Medium',
+        id: '4',
+      },
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Medium',
+        id: '22',
+      },
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Medium',
+        id: '23',
+      },
+    ],
+    'In progress': [
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Low',
+        id: '5',
+      },
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Low',
+        id: '6',
+      },
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Low',
+        id: '7',
+      },
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'Low  ',
+        id: '8',
+      },
+    ],
+    Done: [
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'High',
+        id: '9',
+      },
+      {
+        task: 'Brainstorm ideas for new content or new content',
+        priority: 'High',
+        id: '10',
+      },
+    ],
+  };
+  return (
+    <ChoosedDaySection>
+      {/* <DayCalendarHead /> */}
+      <TasksColumnsList filteredTasks={filteredTasks} />
+    </ChoosedDaySection>
+  );
+}
--- src/components/CalendarPage/ChoosedDay/TasksColumn/TasksColumn.styled.js
+++ src/components/CalendarPage/ChoosedDay/TasksColumn/TasksColumn.styled.js
@@ -1,34 +1,15 @@
 import styled from 'styled-components';
 
-export const TaskColumn = styled.div`
+export const TaskColumn = styled.li`
   width: 335px;
-  max-height: 432px;
-
-  padding: 18px;
+  flex-shrink: 0;
+  padding: 18px 6px 10px 18px;
   background-color: #ffffff;
   border-radius: 8px;
   border: 1px solid rgba(220, 227, 229, 0.5);
 
   @media screen and (min-width: 768px) {
     width: 344px;
-    max-height: 568px;
+    padding: 18px 8px 28px 20px;
   }
-
-  /* &::-webkit-scrollbar {
-    width: 6px;
-  }
-
-  &::-webkit-scrollbar-track {
-    border-radius: 12px;
-    background: #f2f2f2;
-  }
-  
-  &::-webkit-scrollbar-thumb {
-    border-radius: 12px;
-    background: #e7e5e5;
-  }
-
-  &::-webkit-scrollbar-thumb:hover {
-    background: #3e85f3;
-  } */
 `;
