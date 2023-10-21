3f2ae7c9166f007eea3290ca4d1a3b673e3f560b
Gabriella Marusyak <slipuhinag@gmail.com>
2023-10-21 21:17:07 +0300

    Updated files

--- src/components/StatisticsPage/PeriodPaginator/PeriodPaginator.jsx
+++ src/components/StatisticsPage/PeriodPaginator/PeriodPaginator.jsx
@@ -1,6 +1,6 @@
- import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import StyledDatepicker from './StyledDatepicker'
-// import { PaginatorContainer } from './PeriodPaginator.styled';
+
 
  export const PeriodPaginator = ({onDayChange}) =>{
 
