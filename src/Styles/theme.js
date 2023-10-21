570ddf2a032634ce00d9764afe6806dd7c9f6822
Cosmits <salge.cos@gmail.com>
2023-10-21 19:46:35 +0200

    fix

--- src/Styles/theme.js
+++ src/Styles/theme.js
@@ -3,11 +3,8 @@ export const lightTheme = {
   secondaryBgColor: '#FFFFFF',
   mainTextColor: '#111111',
   secondaryTextColor: '#343434',
-  btnActive: '#CAE8FF',
-  btnNoActive: '#E3F3FF',
-  btnTextColor: '#3E85F3',
   mainSideBarText: "rgba(52, 52, 52, 0.50)",
-  popUpBg:'#F6F6F6',
+  popUpBg: '#F6F6F6',
   popUpBgBtn: '#EFEFEF',
 
 
@@ -18,12 +15,9 @@ export const darkTheme = {
   secondaryBgColor: '#21222C',
   mainTextColor: '#FFFFFF',
   secondaryTextColor: '#FFFFFF',
-  btnActiveBGColor:'#3E85F3',
-  btnNoActiveBGcolor:'#21222C',
-  btnActiveTextColor:'#FFFFFF',
-  mainSideBarText:"#FFFFFF",  
-  popUpBg:'#F6F6F6',
+  mainSideBarText: "#FFFFFF",
+  popUpBg: '#F6F6F6',
   popUpBgBtn: '#EFEFEF',
 
-  
+
 };
