76aa9a548063605c9e911854124a1f6e366ae2b6
KaterinaKha <katerinakhaa@gmail.com>
2023-10-22 19:10:18 +0200

    fixing styles, adding x2 img

--- src/components/MainPage/AuthSection/AuthSection.jsx
+++ src/components/MainPage/AuthSection/AuthSection.jsx
@@ -13,15 +13,21 @@ import {
   AuthContainer,
   AuthSectionWrap,
 } from './AuthSection.styled';
-import gooseLogo from '../../../images/Auth_mainpage/GOOSE_logo.png';
+
+import logoRetina from '../../../images/Auth_mainpage/GOOSE_logo_2x.png';
+import logo from '../../../images/Auth_mainpage/GOOSE_logo_1x.png';
+
 import { Container } from '../../../Styles/Container.styled';
 
+import { useMediaQuery } from 'react-responsive';
+
 const AuthSection = () => {
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
   return (
     <AuthSectionWrap>
       <Container>
         <AuthContainer>
-          <AuthLogo src={gooseLogo} alt="Goose Track Logo" />
+          <AuthLogo src={isRetina ? logoRetina : logo} alt="Goose Track Logo" />
           <AuthHeader>
             G<AuthHeaderSpan>oo</AuthHeaderSpan>se Track
           </AuthHeader>
@@ -46,4 +52,4 @@ const AuthSection = () => {
   );
 };
 
-export default AuthSection
+export default AuthSection;
