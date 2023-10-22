f7e338ecf992f9f2833cca2783edd04ebb4a2b58
Cosmits <salge.cos@gmail.com>
2023-10-22 22:33:02 +0200

    fix

--- src/components/MainPage/Description/Description.jsx
+++ src/components/MainPage/Description/Description.jsx
@@ -1,31 +1,26 @@
 import { useMediaQuery } from 'react-responsive';
+import sidebarImg from '../../../images/DescriptionList/mobile-sideBar@1x.jpg'
+import sidebarImgRetina from '../../../images/DescriptionList/mobile-sideBar@2x.jpg';
+import calendarImg from '../../../images/DescriptionList/mobile-calendar@1x.jpg';
+import calendarImgRetina from '../../../images/DescriptionList/mobile-calendar@2x.jpg';
+import taskImg from '../../../images/DescriptionList/mobile-tasks@1x.jpg';
+import taskImgRetina from '../../../images/DescriptionList/mobile-tasks@2x.jpg';
 
-import sidebarImg from '../../../images/Description/mobile-sideBar@1x.jpg';
-import sidebarImgRetina from '../../../images/Description/mobile-sideBar@2x.jpg';
-import calendarImg from '../../../images/Description/mobile-calendar@1x.jpg';
-import calendarImgRetina from '../../../images/Description/mobile-calendar@2x.jpg';
-import taskImg from '../../../images/Description/mobile-tasks@1x.jpg';
-import taskImgRetina from '../../../images/Description/mobile-tasks@2x.jpg';
+import tabletSidebarImg from '../../../images/DescriptionList/tablet-sideBar@1x.jpg';
+import tabletSidebarImgRetina from '../../../images/DescriptionList/tablet-sideBar@2x.jpg'
+import tabletCalendarImg from '../../../images/DescriptionList/tablet-calendar@1x.jpg';
+import tabletCalendarImgRetina from '../../../images/DescriptionList/tablet-calendar@2x.jpg';
+import tabletTaskImg from '../../../images/DescriptionList/tablet-tasks@1x.jpg';
+import tabletTaskImgRetina from '../../../images/DescriptionList/tablet-tasks@2x.jpg';
 
-import tabletSidebarImg from '../../../images/Description/tablet-sideBar@1x.jpg';
-import tabletSidebarImgRetina from '../../../images/Description/tablet-sideBar@2x.jpg'
-import tabletCalendarImg from '../../../images/Description/tablet-calendar@1x.jpg';
-import tabletCalendarImgRetina from '../../../images/Description/tablet-calendar@2x.jpg';
-import tabletTaskImg from '../../../images/Description/tablet-tasks@1x.jpg';
-import tabletTaskImgRetina from '../../../images/Description/tablet-tasks@2x.jpg';
+import deckSidebarImg from '../../../images/DescriptionList/desk-sideBar@1x.jpg';
+import deskSidebarImgRetina from '../../../images/DescriptionList/desk-sideBar@2x.jpg';
+import deskCalendarImg from '../../../images/DescriptionList/desk-calendar@1x.jpg';
+import deskCalendarImgRetina from '../../../images/DescriptionList/desk-calendar@2x.jpg';
+import deskTaskImg from '../../../images/DescriptionList/desk-tasks@1x.jpg';
+import deskTaskImgRetina from '../../../images/DescriptionList/desk-tasks@2x.jpg';
 
-import deckSidebarImg from '../../../images/Description/desk-sideBar@1x.jpg';
-import deskSidebarImgRetina from '../../../images/Description/desk-sideBar@2x.jpg';
-import deskCalendarImg from '../../../images/Description/desk-calendar@1x.jpg';
-import deskCalendarImgRetina from '../../../images/Description/desk-calendar@2x.jpg';
-import deskTaskImg from '../../../images/Description/desk-tasks@1x.jpg';
-import deskTaskImgRetina from '../../../images/Description/desk-tasks@2x.jpg';
-
-import {
-    DescriptionImg, MiddleItem, StyledContainer, StyledDescriptionTitle,
-    StyledDescription, StyledNumberPar, StyledPreTitle, DescriptionList,
-    DescriptionItem, StyledNumber, DescriptionTitle, DescriptionPreTitle
-} from './Description.styled';
+import { DescriptionImg, MiddleItem, StyledContainer, DescriptionTitle, StyledDescription, DescriptionItem, DescriptionList, StyledNumber, DescriptionPreTitle} from './Description.styled';
 import { useScreenSize } from '../../../hooks/useScreenSize';
 
 const Description = () => {
@@ -36,12 +31,11 @@ const Description = () => {
         <DescriptionList>
             <DescriptionItem>
                 <div>
-                    <StyledNumberPar>1.</StyledNumberPar>
-                    <StyledPreTitle>Calendar</StyledPreTitle>
-                    <StyledDescriptionTitle>View</StyledDescriptionTitle>
+                    <StyledNumber>1.</StyledNumber>
+                    <DescriptionPreTitle>Calendar</DescriptionPreTitle>
+                    <DescriptionTitle>View</DescriptionTitle>
                     <StyledDescription>GooseTrack’s Calendar view provides a comprehensive overview of your schedule,
-                        displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.
-                    </StyledDescription>
+                        displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.</StyledDescription>
                 </div>
 
                 {isDesktop && <DescriptionImg src={isRetina ? deskCalendarImgRetina : deskCalendarImg} alt="Calendar" />}
