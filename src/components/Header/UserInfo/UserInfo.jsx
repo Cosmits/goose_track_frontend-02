736c0b8ca15f87327b403cad67649d9334beb3b9
DarinaMerkulova <litovchenko.darina@gmail.com>
2023-10-23 17:51:12 +0300

    userInfo

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -1,30 +1,48 @@
-// import { useSelector } from 'react-redux';
-// import { selectUserData } from 'redux/selectors';
 
-import { useSelector } from 'react-redux';
 import {
- 
+  BackgroundName,
   UserAvatar,
   UserInfoWrapper,
   UserName,
-  } from './UserInfo.styled';
-import { selectUser } from '../../../redux/auth/selectors';
+  UserNameIcon,
+} from './UserInfo.styled';
 
 const UserInfo = () => {
-   const {userName, avatarURL} = useSelector(selectUser);
+  const {userName, avatarURL} = useSelector(selectUser);
 
-   const getFirstName = (userName) => {
+  const getInitials = (userName) => {
+    if (userName) {
+      const initials = userName
+        .split(' ')
+        .map((word) => word.charAt(0))
+        .join('')
+        .toUpperCase();
+      return initials;
+    } else {
+      return userName;
+    }
+  };
+
+  const getFirstName = (userName) => {
     if (userName) {
       return userName.split(' ')[0];
     } else {
       return userName;
     }
   };
-  
-    return (
+
+  const displayName = avatarURL ? (
+    <img src={avatarURL} alt="UserAvatar" />
+  ) : (
+    <BackgroundName>
+      <UserNameIcon>{getInitials(userName)}</UserNameIcon>
+    </BackgroundName>
+  );
+
+  return (
     <UserInfoWrapper>
-      <UserName>{getFirstName(userName)}</UserName>
-      <UserAvatar><img src={avatarURL} alt="Avatar"/></UserAvatar>
+      <UserName>{getFirstName(name)}</UserName>
+      <UserAvatar>{displayName}</UserAvatar>
     </UserInfoWrapper>
   );
 };
