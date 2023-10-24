067d9cf3c3715172b79debb6df732775b371295c
Cosmits <salge.cos@gmail.com>
2023-10-24 12:09:27 +0200

    add UserForm

--- src/components/AccountPage/UserForm/UserForm.styled.jsx
+++ src/components/AccountPage/UserForm/UserForm.styled.jsx
@@ -1,218 +1,341 @@
-// Serhii
+import { AiFillPlusCircle } from 'react-icons/ai';
 import styled from 'styled-components';
-// import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import { HiPlus } from 'react-icons/hi';
-import userSvg from '../../../../src/icons/popUp/user.svg';
-// @media screen and(min - width: 375px) { }
-// @media screen and(min - width: 768px) { }
-// @media screen and(min - width: 1440px) { }
 
-const StyledForm = styled.form`
-  position: relative;
-  margin-top: 95px;
+export const ContainerWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px 40px;
+
+  @media screen and (min-width: 768px) {
+    padding: 0 32px 40px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding: 0 32px 32px;
+  }
+`;
+
+export const Container = styled.div`
+  flex: 1;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: center;
   align-items: center;
-  gap: 40px;
-  width: 100%;
-  padding-top: 59px;
-  padding-bottom: 40px;
+
+  padding: 60px 10px 40px;
+  background-color: ${({ theme }) => theme.secondaryBgColor};
+
   border-radius: 16px;
-  background-color: ${({ theme }) => theme.secondaryBgColor};
+  text-align: center;
+  position: relative;
 
-  @media (min-width: 768px) {
-    gap: 24px;
+  @media screen and (min-width: 768px) {
+    padding: 40px 0;
   }
 
-  @media (min-width: 1440px) {
-  }
-`;
-
-const StyledInputWrapperDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 18px;
-  @media (min-width: 1440px) {
-    flex-direction: row;
-    gap: 50px;
+  @media screen and (min-width: 1440px) {
+    /* padding: 60px 0; */
+    height: 100%;
   }
 `;
 
-const StyledAvatarPlug = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: -31px;
-  width: 124px;
-  height: 124px;
-  padding: 2px;
-  background-color: #3e85f3;
-  fill: red;
-  stroke: black;
-  background-image: url(${userSvg});
-  background-repeat: no-repeat;
-  background-size: 50% 50%;
-  background-position: center;
-  border: 2px solid var(--gradient-blue);
-  border-radius: 50%;
-  z-index: 1;
-`;
-
-const StyledAvatarImgThumb = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  width: 100%;
-  border-radius: 50%;
-  overflow: hidden;
-`;
-
-const StyledAvatarImg = styled.img`
-  position: absolute;
-  align-items: center;
-  display: flex;
-  top: -31px;
-  width: 124px;
-  height: 124px;
-  object-fit: cover;
-  border: 2px solid var(--gradient-blue);
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.secondaryBgColor};
-`;
-
-const StyledAddAvatarBtn = styled(HiPlus)`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  width: 8px;
-  height: 8px;
-  color: var(--white);
-`;
-
-const StyledUserInfoThumb = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 4px;
-`;
-
-const StyledUserNameP = styled.p`
-  margin-top: 59px;
-  /* !--------------------- */
-  font-family: InterTight;
-  /* !--------------------- */
+export const UserName = styled.h2`
+  color: var(--calendar-date-color);
   font-size: 14px;
   font-style: normal;
   font-weight: 700;
   line-height: 18px;
-  color: ${({ theme }) => theme.secondaryTextColor};
+  margin-bottom: 5px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+  }
 `;
 
-const StyledUserStatusP = styled.p`
-  font-family: InterTight;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 14px;
-  color: ${({ theme }) => theme.secondaryTextColor};
+export const Image = styled.img`
+  width: 72px;
+  height: 72px;
+  border-radius: 50%;
+  object-fit: cover;
+
+  @media screen and (min-width: 768px) {
+    width: 124px;
+    height: 124px;
+  }
 `;
 
-const StyledInputSecondWrapperDiv = styled.div`
+export const ImageContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 18px;
-`;
-
-const StyledInputThumbDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  gap: 8px;
-`;
-
-const StyledLabel = styled.label`
-  color: ${({ theme }) => theme.labelTextColor};
-  font-family: 'InterTightRegular', sans-serif;
-  font-size: 12px;
-  font-weight: 400;
-  line-height: 1.16;
-
-  @media (min-width: 768px) {
-    font-size: 14px;
-    line-height: 1.28;
-  }
-`;
-
-const StyledInput = styled.input`
-  width: 100%;
-  height: 42px;
-  padding: 0 14px;
-  font-family: 'InterSemiBold', sans-serif;
-  font-size: 14px;
-  font-weight: 600;
-  line-height: 1.28;
-  border-radius: 8px;
-  border: 1px solid ${({ theme }) => theme.borderInputColor};
-  color: ${({ theme }) => theme.mainTextColor};
-  background-color: ${({ theme }) => theme.secondaryBgColor};
-
-  &::placeholder {
-    color: ${({ theme }) => theme.mainTextColor};
-  }
-
-  @media (min-width: 375px) {
-    width: 299px;
-  }
-
-  @media (min-width: 768px) {
-    width: 354px;
-  }
-`;
-
-const StyledButton = styled.button`
-  display: flex;
-  width: 195px;
-  height: 46px;
   justify-content: center;
   align-items: center;
-  border-radius: 16px;
-  background-color: #3e85f3;
+
+  width: 72px;
+  height: 72px;
+  margin-bottom: 18px;
+
+  background-color: var(--main-background-color);
+  border-radius: 50%;
+  border: 1px solid rgb(47, 103, 228);
+
+  @media screen and (max-width: 767px) {
+    position: absolute;
+    top: -31px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  @media screen and (min-width: 768px) {
+    position: relative;
+    width: 124px;
+    height: 124px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const IconContainer = styled(AiFillPlusCircle)`
+  position: absolute;
+  bottom: -3px;
+  right: 12px;
+  background-color: var(--white);
+  border-radius: 100%;
+  width: 14px;
+  height: 14px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  color: rgb(47, 103, 228);
   border: none;
-  color: #fff;
-  font-family: 'InterSemiBold', sans-serif;
-  font-size: 14px;
-  font-style: normal;
+  cursor: pointer;
+  @media screen and (min-width: 375px) and (max-width: 767px) {
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 24px;
+    height: 24px;
+    right: 18px;
+    bottom: -4px;
+  }
+`;
+
+export const InputWrapper = styled.div`
+  @media screen and (min-width: 1440px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    align-items: start;
+    justify-items: center;
+    flex: 1;
+
+    grid-column-gap: 50px;
+  }
+`;
+
+export const Text = styled.p`
+  color: var(--calendar-date-color);
+  font-size: 12px;
   font-weight: 600;
-  line-height: 18px;
+  line-height: 14px;
+  margin-bottom: 40px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+  }
+`;
+
+export const Letter = styled.p`
+  color: var(--accent-color);
+  font-weight: 700;
+  font-size: 33px;
+  line-height: 1.28;
+
+  @media screen and (min-width: 768px) {
+    font-size: 50px;
+  }
+`;
+
+export const Forma = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  & > div > div {
+    display: flex;
+    flex-direction: column;
+
+    @media screen and (max-width: 768px) {
+      gap: 18px;
+    }
+
+    @media screen and (min-width: 768px) {
+      gap: 24px;
+    }
+  }
+
+  & > div > div:nth-of-type(1) {
+    margin-bottom: 18px;
+
+    @media screen and (min-width: 768px) {
+      margin-bottom: 24px;
+    }
+  }
+
+  & > div > div > label {
+    display: flex;
+    flex-direction: column;
+    align-items: baseline;
+  }
+
+  & > div > div > label > input {
+    width: 100%;
+    height: 42px;
+    border-radius: 8px;
+    border: 1px solid #e0e0e0;
+    padding: 12px 14px;
+    font-size: 14px;
+    line-height: 18px;
+    font-weight: 600;
+    color: var(--calendar-date-color);
+    outline: none;
+    background-color: inherit;
+
+    &::placeholder {
+      color: var(--calendar-date-color);
+      font-size: 14px;
+      line-height: 18px;
+      font-weight: 400;
+    }
+
+    @media screen and (min-width: 375px) and (max-width: 767px) {
+      width: 299px;
+    }
+
+    @media screen and (min-width: 768px) {
+      width: 354px;
+      height: 46px;
+    }
+  }
+
+  & > div > div > label > p {
+    color: var(--title-text-main-color);
+    font-size: 12px;
+    line-height: 14px;
+    margin-bottom: 8px;
+  }
+
+  & > div > div > div > label {
+    display: flex;
+    flex-direction: column;
+    align-items: baseline;
+  }
+
+  & > div > div > div > label > input {
+    width: 100%;
+    height: 42px;
+    border-radius: 8px;
+    border: 1px solid #e0e0e0;
+    padding: 12px 14px;
+    font-size: 14px;
+    line-height: 18px;
+    font-weight: 600;
+    color: var(--calendar-date-color);
+    outline: none;
+    background-color: inherit;
+
+    &::placeholder {
+      color: var(--calendar-date-color);
+      font-size: 14px;
+      line-height: 18px;
+      font-weight: 400;
+    }
+
+    @media screen and (min-width: 375px) and (max-width: 767px) {
+      width: 299px;
+    }
+
+    @media screen and (min-width: 768px) {
+      width: 354px;
+      height: 46px;
+    }
+  }
+
+  & > div > div > div > label > p {
+    color: var(--label-text-color);
+    font-size: 12px;
+    line-height: 14px;
+    margin-bottom: 8px;
+  }
+`;
+
+
+export const Button = styled.button`
+  max-width: 195px;
+  height: 46px;
+  border-radius: 16px;
+  background-color: var(--main-blue, #3e85f3);
+  border: none;
+  padding: 14px 50px;
+  color: var(--white);
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 17px;
+  cursor: pointer;
+  outline: none;
+  margin-top: 40px;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
   &:hover,
   &:focus {
-    background-color: #2b78ef;
+    background-color: var(--hover-blue);
+    box-shadow: 4px 2px 16px 0px rgba(136, 165, 191, 0.48);
   }
   &:disabled {
-    background-color: red;
+    background-color: #3E85F380;
+    cursor: not-allowed;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 262px;
+    height: 48px;
+    padding: 10px 20px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    margin-top: auto;
+    grid-column: span 2;
   }
 `;
 
-export {
-  StyledAvatarImgThumb,
-  StyledAvatarImg,
-  StyledAvatarPlug,
-  StyledAddAvatarBtn,
-  StyledUserInfoThumb,
-  StyledUserNameP,
-  StyledUserStatusP,
-  StyledForm,
-  StyledInputWrapperDiv,
-  StyledInputSecondWrapperDiv,
-  StyledInputThumbDiv,
-  StyledLabel,
-  StyledInput,
-  StyledButton,
-};
+export const CustomInput = styled.input`
+  width: 100%;
+  height: 42px;
+  border-radius: 8px;
+  border: 1px solid #e0e0e0;
+  padding: 12px 14px;
+  font-size: 14px;
+  line-height: 18px;
+  font-weight: 600;
+  color: var(--calendar-date-color);
+  outline: none;
+  background-color: inherit;
+
+  &::placeholder {
+    color: var(--modal-input-lable-color);
+    font-size: 14px;
+    line-height: 18px;
+    font-weight: 400;
+  }
+
+  @media screen and (min-width: 375px) and (max-width: 767px) {
+    width: 299px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 354px;
+    height: 46px;
+  }
+`;
+
+
