819661e847e32ae123462281567ce9b74a81f180
Ruslana Onyshchuk <lalelya7@gmail.com>
2023-10-24 15:41:45 +0200

    added x-close svg to taskform

--- src/components/TaskForm/TaskForm.styled.js
+++ src/components/TaskForm/TaskForm.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 import { ReactComponent as AddTaskBtnIcon } from '../../icons/ChoosedDay/add-task-btn-plus.svg.svg';
 import { ReactComponent as Edit } from '../../icons/ChoosedDay/task-card-edit.svg';
+import { ReactComponent as XClose } from '../../icons/popUp/x-close.svg';
 
 export const PencilIcon = styled(Edit)`
   width: 18px;
@@ -17,7 +18,21 @@ export const AddTask = styled(AddTaskBtnIcon)`
   cursor: pointer;
 `;
 
+export const CloseIcon = styled(XClose)`
+  width: 24px;
+  height: 24px;
+  stroke: #111;
+  cursor: pointer;
+  margin-right: auto;
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
 export const ContainerForm = styled.div`
+  width: 100%;
+  /* height:360px; */
+  position: relative;
   margin: 0 auto;
   border-radius: 8px;
   border: rgba(220, 227, 229, 0.8);
