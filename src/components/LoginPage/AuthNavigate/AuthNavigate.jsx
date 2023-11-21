533739b2dedcb5cf9dee283c7a6e1630a27d39d5
Cosmits <salge.cos@gmail.com>
2023-11-21 16:44:45 +0100

    refactoringCode

--- src/components/LoginPage/AuthNavigate/AuthNavigate.jsx
+++ src/components/LoginPage/AuthNavigate/AuthNavigate.jsx
@@ -2,8 +2,8 @@
 
 import { AuthNavLink } from "./AuthNavigate.styled";
 
-const AuthNavigate = ({route, textContent}) => {
-       return <AuthNavLink to={route}>{textContent}</AuthNavLink>
+const AuthNavigate = ({ route, textContent }) => {
+  return <AuthNavLink to={route}>{textContent}</AuthNavLink>
 };
 
 export default AuthNavigate;
--- src/components/SendVerifyEmailPage/SendVerifyEmailForm.jsx
+++ src/components/SendVerifyEmailPage/SendVerifyEmailForm.jsx
@@ -6,8 +6,8 @@ import { useDispatch } from 'react-redux';
 
 import { sendVerifyEmailUser } from '../../redux/auth/operations';
 import {
-    Button, Error, Icon, Title,
-    Input, Label, LogInPicture, VerifyEmailInput
+  Button, Error, Icon, Title,
+  Input, Label, LogInPicture, VerifyEmailInput
 } from '../RegisterPage/RegisterForm/RegisterForm.styled';
 import SuccessIcon from 'images/RegisterPage/success.svg';
 import ErrorIcon from 'images/RegisterPage/error.svg';
@@ -16,77 +16,76 @@ import { globalRegex } from '../../Styles/GlobalStyles';
 import { useNavigate } from 'react-router';
 
 const ErrorMessages = ({ name }) => {
-    return (
-        <ErrorMessage
-            name={name}
-            render={message => (<Error>{message}</Error>)}
-        />
-    );
+  return (
+    <ErrorMessage
+      name={name}
+      render={message => (<Error>{message}</Error>)}
+    />
+  );
 };
 
 const initialValues = {
-    email: '',
+  email: '',
 };
 
 const schema = yup.object().shape({
-    email: yup.string()
-        .matches(globalRegex.emailRegexp,
-            'Email must contain only digits, letters and . - _ symbols: "example@ukr.net"',)
-        .email('Invalid email, enter in format "example@ukr.net"')
-        .required('Email is required'),
+  email: yup.string()
+    .matches(globalRegex.emailRegexp,
+      'Email must contain only digits, letters and . - _ symbols: "example@ukr.net"',)
+    .email('Invalid email, enter in format "example@ukr.net"')
+    .required('Email is required'),
 });
 
 const SendVerifyEmailForm = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const handleVerifySubmit = async (values, { resetForm }) => {
-        const { email } = values;
+  const handleVerifySubmit = async (values, { resetForm }) => {
+    const { email } = values;
 
-        dispatch(sendVerifyEmailUser({ email }))
-        resetForm();
-        navigate(`/goose_track_frontend-02/login`)
+    dispatch(sendVerifyEmailUser({ email }))
+    resetForm();
+    navigate(`/goose_track_frontend-02/login`)
+  };
 
-    };
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={handleVerifySubmit}
+    >
+      {({ values, errors, touched }) => (
+        <Form>
+          <Title>Verify Email</Title>
+          <VerifyEmailInput>
+            <Label
+              htmlFor="email"
+              className={touched.email ? errors.email ? 'error' : 'success' : ''}>Email
+            </Label>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              autoComplete="off"
+              placeholder="Enter email"
+              className={touched.email ? errors.email ? 'error' : 'success' : ''}
+            />
+            <ErrorMessages name="email" />
+            {errors.email && touched.email
+              ? (<Icon src={ErrorIcon} />)
+              : values.email && !errors.email
+                ? (<Icon src={SuccessIcon} />)
+                : null}
+          </VerifyEmailInput>
 
-    return (
-        <Formik
-            initialValues={initialValues}
-            validationSchema={schema}
-            onSubmit={handleVerifySubmit}
-        >
-            {({ values, errors, touched }) => (
-                <Form>
-                    <Title>Verify Email</Title>
-                    <VerifyEmailInput>
-                        <Label
-                            htmlFor="email"
-                            className={touched.email ? errors.email ? 'error' : 'success' : ''}>Email
-                        </Label>
-                        <Input
-                            type="email"
-                            id="email"
-                            name="email"
-                            autoComplete="off"
-                            placeholder="Enter email"
-                            className={touched.email ? errors.email ? 'error' : 'success' : ''}
-                        />
-                        <ErrorMessages name="email" />
-                        {errors.email && touched.email
-                            ? (<Icon src={ErrorIcon} />)
-                            : values.email && !errors.email
-                                ? (<Icon src={SuccessIcon} />)
-                                : null}
-                    </VerifyEmailInput>
-
-                    <Button type="submit">
-                        Verify Email
-                        <LogInPicture src={LogInIcon} />
-                    </Button>
-                </Form>
-            )}
-        </Formik>
-    );
+          <Button type="submit">
+            Verify Email
+            <LogInPicture src={LogInIcon} />
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default SendVerifyEmailForm;
